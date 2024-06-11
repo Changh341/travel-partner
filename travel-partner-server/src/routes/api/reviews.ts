001cbca754122b32afbcb8a8613939d551703607
howard <changh341@gmail.com>
2024-06-10 19:23:09 -0700

    Added update+create routes for reviews

--- travel-partner-server/src/routes/api/reviews.ts
+++ travel-partner-server/src/routes/api/reviews.ts
@@ -13,3 +13,34 @@ router.get("/:attrId", async (req: Request, res: Response, next) => {
   if (reviews.length) return res.send(reviews);
   next();
 });
+
+router.post("/:attrId", async (req: Request, res: Response, next) => {
+  const { attrId } = req.params;
+  const { body, star, userId } = req.body;
+  const review = await prisma.review.create({
+    data: {
+      attrId,
+      body,
+      star,
+      userId,
+      approved: "PENDING",
+    },
+  });
+  if (review) return res.send(review);
+  next();
+});
+
+router.patch("/:reviewId", async (req: Request, res: Response, next) => {
+  const { reviewId } = req.params;
+  const { body, star, userId } = req.body;
+  const review = await prisma.review.update({
+    where: { id: reviewId },
+    data: {
+      body,
+      star,
+      approved: "PENDING",
+    },
+  });
+  if (review) return res.send(review);
+  next();
+});
